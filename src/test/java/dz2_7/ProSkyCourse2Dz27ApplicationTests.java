76b0937895c0fb5008a1c64f69d1a87986ddaee9
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-07-10 14:08:02 +0500

    Initial commit

---
+++ src/test/java/dz2_7/ProSkyCourse2Dz27ApplicationTests.java
@@ -0,0 +1,13 @@
+package dz2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSkyCourse2Dz27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
